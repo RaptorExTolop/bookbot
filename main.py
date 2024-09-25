a1d9e9779e78e318e52453d0b4a30185cbe09215
RaptorExTolop-School <jamesappletonret@gmail.com>
2024-09-25 11:24:59 +0930

    2: main.py reads file and gives word count

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+file_contents = None
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    word_count = count_words(file_contents)
+    print(word_count)
+
+def count_words(c):
+    total_words = 0
+    c = c.split()
+    for i in range(len(c)):
+        total_words += 1
+    return total_words
+
+main()
