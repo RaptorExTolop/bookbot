10549a727e444c92e5b820a166dcb4151bd14128
RaptorExTolop-School <jamesappletonret@gmail.com>
2024-09-25 16:35:37 +0930

    3: added cout_chars funtion

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
         file_contents = f.read()
     word_count = count_words(file_contents)
     print(word_count)
+    print(count_chars(file_contents))
 
 def count_words(c):
     total_words = 0
@@ -12,5 +13,16 @@ def count_words(c):
     for i in range(len(c)):
         total_words += 1
     return total_words
+
+def count_chars(c):
+    NOEC = {} # Numbers Of Each Chars
+    for i in c:
+        if i == ' ' or i == '\n':
+            pass
+        elif i in NOEC:
+            NOEC[i] += 1
+        else:
+            NOEC[i] = 0
+    return NOEC
 
 main()
